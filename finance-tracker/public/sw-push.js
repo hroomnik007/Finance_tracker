aa7abded6989550a29c3ec3ab32243eb549d1cca
hroomnik <peter.bliznak@protonmail.com>
2026-05-06 23:39:10 +0200

    fix: notification click opens PWA instead of browser

--- finance-tracker/public/sw-push.js
+++ finance-tracker/public/sw-push.js
@@ -12,5 +12,14 @@ self.addEventListener('push', event => {
 
 self.addEventListener('notificationclick', event => {
   event.notification.close()
-  event.waitUntil(clients.openWindow(event.notification.data.url))
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+      for (const client of clientList) {
+        if (client.url.includes('financie.pedani.eu') && 'focus' in client) {
+          return client.focus()
+        }
+      }
+      return clients.openWindow('https://financie.pedani.eu/')
+    })
+  )
 })
